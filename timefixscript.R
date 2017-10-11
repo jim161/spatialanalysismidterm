726a0809e931d2b0b623a426f9b17c5250da0452
jim161 <jonas.mendezreneau1@louisiana.edu>
2017-10-11 16:25:18 -0500

    Joined date and time in one column for ARCGIS format

--- timefixscript.R
+++ timefixscript.R
@@ -8,3 +8,9 @@ library("oce")
 X195101_201404_ZoopXL$time <- X195101_201404_ZoopXL$Tow_Time
 
 X195101_201404_ZoopXL$time <- gsub("1899-12-31 ", "", X195101_201404_ZoopXL$time)
+
+#Joining date and time
+date<- X195101_201404_ZoopXL$Tow_Date
+time<- X195101_201404_ZoopXL$time
+
+X195101_201404_ZoopXL$DateTime <- str_c(date,time,sep=" ")
