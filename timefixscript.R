edb3d5e3e80f653b0b37c982bf1a3ba94c386c91
jim161 <jonas.mendezreneau1@louisiana.edu>
2017-10-11 17:42:25 -0500

    isolated year in CalCO data and added write.table commands for both datasets

--- timefixscript.R
+++ timefixscript.R
@@ -14,3 +14,11 @@ date<- X195101_201404_ZoopXL$Tow_Date
 time<- X195101_201404_ZoopXL$time
 
 X195101_201404_ZoopXL$DateTime <- str_c(date,time,sep=" ")
+
+write.table(X195101_201404_ZoopXL, "Zoop.txt", sep="\t", row.names =FALSE, col.names=TRUE)
+
+#isolating date to get points from 1997/1998 and make table
+trawlyear<- str_sub(erdCalCOFIcufes_bb4a_5c83_ad3aXL$time_UTC ,1,4)
+erdCalCOFIcufes_bb4a_5c83_ad3aXL$YEAR <- trawlyear
+
+write.table(erdCalCOFIcufes_bb4a_5c83_ad3aXL, "CalCO.txt", sep="\t", row.names =FALSE, col.names=TRUE)
