f0863aeb8881091b954e0b15bfcd8d6bb0b019b2
jim161 <jonas.mendezreneau1@louisiana.edu>
2017-10-11 20:10:53 -0500

    #coordinate conversion to decimal degrees and timezone fix

--- timefixscript.R
+++ timefixscript.R
@@ -3,6 +3,7 @@ library("plyr")
 library("stringr")
 library("pastecs")
 library("oce")
+library("measurements")
 
 #Got time isolated with out arbitrary date
 X195101_201404_ZoopXL$time <- X195101_201404_ZoopXL$Tow_Time
@@ -14,6 +15,17 @@ date<- X195101_201404_ZoopXL$Tow_Date
 time<- X195101_201404_ZoopXL$time
 
 X195101_201404_ZoopXL$DateTime <- str_c(date,time,sep=" ")
+
+#timezone fix
+X195101_201404_ZoopXL$DateTimeLAtz <- as.POSIXct(strptime(X195101_201404_ZoopXL$DateTime, tz = "America/Los_Angeles", format=X195101_201404_ZoopXL$DateTime))
+
+#converting to decimal degrees(LONGDec and LATDec)
+X195101_201404_ZoopXL$LatDegDate <- str_c(X195101_201404_ZoopXL$Lat_Deg, X195101_201404_ZoopXL$Lat_Min, sep=" ")
+X195101_201404_ZoopXL$LonDegDate <- str_c(X195101_201404_ZoopXL$Lon_Deg, X195101_201404_ZoopXL$Lon_Min, sep=" ")
+
+
+X195101_201404_ZoopXL$LATDec <- measurements::conv_unit(X195101_201404_ZoopXL$LatDegDate, from = 'deg_dec_min' , to = 'dec_deg')
+X195101_201404_ZoopXL$LONGDec <- measurements::conv_unit(X195101_201404_ZoopXL$LonDegDate, from = 'deg_dec_min', to = 'dec_deg')
 
 write.table(X195101_201404_ZoopXL, "Zoop.txt", sep="\t", row.names =FALSE, col.names=TRUE)
 
